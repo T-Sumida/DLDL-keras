080eca4e4e7a79c363b80bd793ea795839c67a06
T-Sumida <ui.suto05@gmail.com>
2022-01-03 16:39:51 +0900

    [add] add example script

---
+++ example.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+
+from dldl_v2.model import build_model
+
+
+if __name__ == "__main__":
+
+    INPUT_SHAPE = (224, 224, 3)
+    LEARNING_RATE = 1e-3
+
+    model = build_model(INPUT_SHAPE)
+    optimizer = Adam(learning_rate=LEARNING_RATE, clipnorm=1., epsilon=1e-8)
+    model.compile(optimizer=optimizer, loss=[tf.keras.losses.kullback_leibler_divergence, tf.keras.losses.mean_absolute_error])
+    print(model.summary())
